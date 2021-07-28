ea75fa59489025ddeed89ed3a207b19fc7db1bd4
TasneemZh <tasneemisfree2@gmail.com>
2021-07-28 13:33:50 +0300

    Enable CORS

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const mailchimp = require("@mailchimp/mailchimp_marketing");
 const express = require("express");
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.urlencoded({
   extended: true
 }));
